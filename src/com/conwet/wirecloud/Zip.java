99e665032681a54a40ccec69bb26850dddb95f5d
Álvaro Arranz <aarranz@conwet.com>
2013-12-18 17:11:10 +0100

    Add copyright headers

--- src/com/conwet/wirecloud/Zip.java
+++ src/com/conwet/wirecloud/Zip.java
@@ -1,25 +1,21 @@
 /*
- *     Copyright (c) 2013 CoNWeT Lab., Universidad Politécnica de Madrid
- *
- *     This file is part of the Wirecloud IDE Project,
- *
- *     http://conwet.fi.upm.es/wirecloud/ide
- *
- *     Licensed under the GNU General Public License, Version 3.0 (the 
- *     "License"); you may not use this file except in compliance with the 
- *     License.
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     under the License is distributed in the hope that it will be useful, 
- *     but on an "AS IS" BASIS, WITHOUT ANY WARRANTY OR CONDITION,
- *     either express or implied; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  Copyright (c) 2013 CoNWeT Lab., Universidad Politécnica de Madrid
  *  
- *     See the GNU General Public License for specific language governing
- *     permissions and limitations under the License.
+ *  This file is part of Wirecloud IDE.
  *
- *     <http://www.gnu.org/licenses/gpl.txt>.
+ *  Wirecloud IDE is free software: you can redistribute it and/or modify
+ *  it under the terms of the European Union Public Licence (EUPL)
+ *  as published by the European Commission, either version 1.1
+ *  of the License, or (at your option) any later version.
  *
+ *  Wirecloud IDE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  European Union Public Licence for more details.
+ *
+ *  You should have received a copy of the European Union Public Licence
+ *  along with Wirecloud IDE.
+ *  If not, see <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>.
  */
 
 package com.conwet.wirecloud;
